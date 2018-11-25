0a3757fc6615fefed78ca808a22924f1337b867e
Slawomir Chyla <chylaslawek@gmail.com>
2018-11-25 10:52:31 +0100

    Added first policy

--- Include/Logger.hpp
+++ Include/Logger.hpp
@@ -3,19 +3,60 @@
 
 enum class LogLevel { Debug, Info, Warning, Error };
 
+class ConsoleLogPolicy
+{
+protected:
+	template <class T> friend class Logger;
+
+	void write(const std::string& buffer)
+	{
+		std::cout << buffer;
+	}
+};
+
+template <LogLevel Level>
+std::string logLevelToStr()
+{
+    return std::string("Default log level implementation");
+}
+template <>
+std::string logLevelToStr<LogLevel::Info>()
+{
+    return std::string("[INFO] ");
+}
+
+template <>
+std::string logLevelToStr<LogLevel::Debug>()
+{
+    return std::string("[DEBUG] ");
+}
+
+template <>
+std::string logLevelToStr<LogLevel::Warning>()
+{
+    return std::string("[WARNING] ");
+}
+
+template <>
+std::string logLevelToStr<LogLevel::Error>()
+{
+    return std::string("[ERROR] ");
+}
+
+template <class Policy>
 class Logger
 {
 public:
-	Logger(std::ostream& stream) : output(stream) {}
-	Logger(std::string filename) : output(std::cout)
-	{
-		file.open(filename);
-	}
+	Logger() {}
 
 	template <LogLevel Level>
-	void log(std::string);
-
-private:
-	std::ostream& output;
-	std::ofstream file;
+	void log(std::string str)
+	{
+		std::string logLevelStr;
+		logLevelStr = logLevelToStr<Level>();
+		Policy p;
+		p.write(logLevelStr + str);
+	}
 };
+
+
--- Test/LoggerTestSuite.cpp
+++ Test/LoggerTestSuite.cpp
@@ -5,30 +5,30 @@
 
 TEST(LoggerTestSuite, CoutLoggerTestDebug)
 {
-	Logger logger(std::cout);
+	Logger<ConsoleLogPolicy> logger;
 	logger.log<LogLevel::Debug>("Some text");
 }
 
 TEST(LoggerTestSuite, CoutLoggerTestInfo)
 {
-	Logger logger(std::cout);
+	Logger<ConsoleLogPolicy> logger;
 	logger.log<LogLevel::Info>("Some text");
 }
 
 TEST(LoggerTestSuite, CoutLoggerTestWarning)
 {
-	Logger logger(std::cout);
+	Logger<ConsoleLogPolicy> logger;
 	logger.log<LogLevel::Warning>("Some text");
 }
 
 TEST(LoggerTestSuite, CoutLoggerTestError)
 {
-	Logger logger(std::cout);
+	Logger<ConsoleLogPolicy> logger;
 	logger.log<LogLevel::Error>("Some text");
 }
 
-TEST(LoggerTestSuite, FileLoggerTestOneClass)
+/*TEST(LoggerTestSuite, FileLoggerTestOneClass)
 {
 	Logger logger("tmp.log");
 	logger.log<LogLevel::Info>("Some text");
-}
+}*/
